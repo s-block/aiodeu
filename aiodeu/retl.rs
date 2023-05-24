7fbb32280e10a732d694d38fe9c699133ffb1bb9
Josh Rowe <josh@s-block.com>
2023-05-24 17:23:42 +0100

    Test rust

--- aiodeu/retl.rs
+++ aiodeu/retl.rs
@@ -1,99 +1,69 @@
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
-use pyo3::types::{PyDict, PyList, PyFloat, PyInt, PyString, PyAny, PyLong};
-
+use pyo3::types::{PyDict, PyList, PyAny};
 use std::collections::HashMap;
 
-// enum PyRecord {
-//     // None,
-//     String(PyString),
-//     Integer(PyInt),
-//     Float(PyFloat),
-//     Long(PyLong),
-//     // List(PyList),
-//     // Dict(PyDict)
-// }
+fn _get_hm_value(py: Python<'_>, hm: &HashMap<String, Py<PyAny>>, field: &str, dp: &String) -> PyObject {
+    let val: Option<&Py<PyAny>> = hm.get(field);
+    match val {
+        Some(v) => {
+            _get_field(py, dp.as_str(), v.as_ref(py))
+        },
+        None => "".to_object(py),
+    }
+}
 
-// fn get_dict_value(py: Python, first_field: &str, dp: &str, dict: PyDict) -> PyRecord {
-//     let val = dict.get_item(first_field);
-//     return _get_field(dp, &val);
-// }
-//
-// fn get_list_value(py: Python, dp: &str, list: PyList) -> PyRecord {
-//     if !list.is_empty() {
-//         let val = list.get_item(0);
-//         return _get_field(dp, val);
-//     }
-//     PyString::new(py, "");
-// }
+fn _get_vec_value(py: Python<'_>, v: &Vec<Py<PyAny>>, field: &str, dp: &String) -> PyObject {
+    if !v.is_empty() {
+        let hm: Result<HashMap<String, Py<PyAny>>, PyErr> = v[0].extract::<HashMap<String, PyObject>>(py);
+        match hm {
+            Ok(d) => {
+                _get_hm_value(py, &d, field, &dp)
+            },
+            Err(_) => "".to_object(py)
+        }
+    } else {
+        "".to_object(py)
+    }
+}
 
-fn _get_field(dot_path: &str, record: PyObject) -> PyObject {
+fn _get_field(py: Python<'_>, dot_path: &str, any: &PyAny) -> PyObject {
     let field_parts: Vec<&str> = dot_path.split(".").collect();
-
-    let first_field = field_parts[0];
-    let remaining_fields = &field_parts[1..];
-    let dp = remaining_fields.join(".");
-
-    // match record {
-    //     None => PyString::new(py, ""),
-    //     Some(PyRecord::String(s)) => s,
-    //     Some(PyRecord::Long(l)) => l,
-    //     Some(PyRecord::Integer(i)) => i,
-    //     Some(PyRecord::Float(f)) => f,
-    //     // Some(PyRecord::Dict(d)) => get_dict_value(py, first_field, dp, d),
-    //     // Some(PyRecord::List(l)) => get_list_value(py, dp, l),
-    // }
-    record
+    let field = field_parts[0];
+    if any.is_none() {
+        "".to_object(py)
+    } else if field.is_empty() {
+        any.to_object(py)
+    } else {
+        let remaining_fields = &field_parts[1..];
+        let dp = remaining_fields.join(".");
+        if let Ok(b) = py.is_instance::<PyDict, _>(any) {
+            if b {
+                let hm: HashMap<String, Py<PyAny>> = any.extract::<HashMap<String, PyObject>>().unwrap();
+                return _get_hm_value(py, &hm, field, &dp);
+            }
+        }
+        if let Ok(b) = py.is_instance::<PyList, _>(any) {
+            if b {
+                let v: Vec<Py<PyAny>> = any.extract::<Vec<PyObject>>().unwrap();
+                return _get_vec_value(py, &v, field, &dp);
+            }
+        }
+        any.to_object(py)
+        // if let Ok(hm) = any.extract::<HashMap<String, PyObject>>() {
+        //     _get_hm_value(py, &hm, field, &dp)
+        // } else if let Ok(v) = any.extract::<Vec<PyObject>>() {
+        //     _get_vec_value(py, &v, field, &dp)
+        // } else {
+        //     any.to_object(py)
+        // }
+    }
 }
 
 #[pyfunction(dot_path, record, name = "get_field")]
 fn get_field(py: Python<'_>, dot_path: &str, record: &PyAny) -> PyResult<PyObject> {
-    let field_parts: Vec<&str> = dot_path.split(".").collect();
-    let field = field_parts[0];
-    let remaining_fields = &field_parts[1..];
-    let dp = remaining_fields.join(".");
-
-    if field.is_empty() {
-        if record.is_none() {
-            Ok("".to_object(py))
-        } else {
-            Ok(record.to_object(py))
-        }
-    } else {
-        let dict = record.extract::<HashMap<String, PyObject>>();
-        match dict {
-            Ok(d) => {
-                let val = d.get(field);
-                match val {
-                    Some(v) => return get_field(py, dp.as_str(), v.as_ref(py)),
-                    None => return Ok("".to_object(py)),
-                }
-            }
-            Err(_) => (),
-        }
-
-        let list = record.extract::<Vec<PyObject>>();
-        match list {
-            Ok(l) => {
-                if !l.is_empty() {
-                    let d = l[0].extract::<HashMap<String, PyObject>>(py).unwrap();
-                    let val = d.get(field);
-                    match val {
-                        Some(v) => return get_field(py, dp.as_str(), v.as_ref(py)),
-                        None => return Ok("".to_object(py)),
-                    }
-                }
-            }
-            Err(_) => (),
-        }
-
-        if record.is_none() {
-            return Ok("".to_object(py));
-        }
-        Ok(record.to_object(py))
-    }
+    Ok(_get_field(py, dot_path, &record))
 }
-
 
 #[pymodule]
 fn retl(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
